e3b14fd5f8ebcd38c00ad1d25df9070ae1db6e8f
Chuck <chuck.peperanney@gmail.com>
2015-03-12 17:05:30 -0400

    covert

--- latex2docx.sh
+++ latex2docx.sh
@@ -1,6 +1,6 @@
 bib="bibliography/biblio.bib"
 csl="pnas.csl"
 
-for f in Introduction.tex Results.tex Discussion.tex Methods.tex; do
+for f in Abstract.tex Introduction.tex Results.tex Discussion.tex Methods.tex Supplemental_notes.tex; do
     pandoc -o ${f%.*}.doc $f --bibliography $bib --csl $csl
 done

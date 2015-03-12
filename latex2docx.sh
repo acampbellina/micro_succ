245d079dd4c4f9936f6e91b804cd59dc1920c0ab
Chuck <chuck.peperanney@gmail.com>
2015-03-12 15:34:17 -0400

    pnas csl

--- latex2docx.sh
+++ latex2docx.sh
@@ -1,5 +1,5 @@
 bib="bibliography/biblio.bib"
-csl="styles/pnas.csl"
+csl="pnas.csl"
 
 for f in Introduction.tex Results.tex Discussion.tex Methods.tex; do
     pandoc -o ${f%.*}.doc $f --bibliography $bib --csl $csl

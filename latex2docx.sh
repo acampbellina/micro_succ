e07900b55ee7e7e49765db7bdde3bcf90c3eac26
Chuck <chuck.peperanney@gmail.com>
2015-03-29 21:55:12 -0400

    l2d change

--- latex2docx.sh
+++ latex2docx.sh
@@ -1,6 +1,6 @@
 bib="bibliography/biblio.bib"
 csl="pnas.csl"
 
-for f in Abstract.tex Introduction.tex Results.tex Discussion.tex Methods.tex Supplemental_notes.tex; do
+for f in Significance.tex Abstract.tex Introduction.tex Results.tex Discussion.tex Methods.tex Supplemental_notes.tex; do
     pandoc -o ${f%.*}.doc $f --bibliography $bib --csl $csl
 done

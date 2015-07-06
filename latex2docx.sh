a43ea820dfd1efb965999cd6bfcbecfe3f9e4dae
Chuck <chuck.peperanney@gmail.com>
2015-07-06 15:46:05 -0400

    figure refs

--- latex2docx.sh
+++ latex2docx.sh
@@ -14,37 +14,44 @@ cat supplemental.tex supp_captions.tex \
 
 sed -i 's/\[fig:ord\]/1/g' manuscript.doc
 sed -i 's/\[fig:l2fc\]/2/g' manuscript.doc
-sed -i 's/\[fig:shift\]/3/g' manuscript.doc
-sed -i 's/\[fig:genspec\]/4/g' manuscript.doc
-sed -i 's/\[fig:example\]/5/g' manuscript.doc
-sed -i 's/\[fig:tiledtree\]/6/g' manuscript.doc
+sed -i 's/\[fig:tiledtree\]/3/g' manuscript.doc
+sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/4/g' manuscript.doc
+sed -i 's/\[fig:shift\]/5/g' manuscript.doc
 
-sed -i 's/\[fig:13C\]/S1/g' manuscript.doc
-sed -i 's/\[fig:setup\]/S2/g' manuscript.doc
-sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S3/g' manuscript.doc
-sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S4/g' manuscript.doc
-sed -i 's/\[fig:time\]/S5/g' manuscript.doc
-sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S6/g' manuscript.doc
-sed -i 's/\[fig:babund\]/S7/g' manuscript.doc
-sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/S8/g' manuscript.doc
-sed -i 's/\[fig:c1\]/S9/g' manuscript.doc
+sed -i 's/\[tab:xyl\]/S1/g' manuscript.doc
+sed -i 's/\[tab:cell\]/S2/g' manuscript.doc
+
+sed -i 's/\[fig:setup\]/S1/g' manuscript.doc
+sed -i 's/\[fig:13C\]/S2/g' manuscript.doc
+sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S3/g' manuscript.doc
+sed -i 's/\[fig:time\]/S4/g' manuscript.doc
+sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S5/g' manuscript.doc
+sed -i 's/\[fig:babund\]/S6/g' manuscript.doc
+sed -i 's/\[fig:genspec\]/S7/g' manuscript.doc
+sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S8/g' manuscript.doc
+sed -i 's/\[fig:example\]/S9/g' manuscript.doc
 sed -i 's/\[fig:copy\]/S10/g' manuscript.doc
-sed -i 's/\[fig:foodweb\]/S11/g' manuscript.doc
+sed -i 's/\[fig:c1\]/S11/g' manuscript.doc
+sed -i 's/\[fig:c2\]/S12/g' manuscript.doc
 
 sed -i 's/\[fig:ord\]/1/g' supplemental.doc
 sed -i 's/\[fig:l2fc\]/2/g' supplemental.doc
-sed -i 's/\[fig:shift\]/3/g' supplemental.doc
-sed -i 's/\[fig:genspec\]/4/g' supplemental.doc
+sed -i 's/\[fig:tiledtree\]/3/g' supplemental.doc
+sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/4/g' supplemental.doc
+sed -i 's/\[fig:shift\]/5/g' supplemental.doc
 
-sed -i 's/\[fig:13C\]/S1/g' supplemental.doc
-sed -i 's/\[fig:setup\]/S2/g' supplemental.doc
-sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S3/g' supplemental.doc
-sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S4/g' supplemental.doc
-sed -i 's/\[fig:time\]/S5/g' supplemental.doc
-sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S6/g' supplemental.doc
-sed -i 's/\[fig:babund\]/S7/g' supplemental.doc
-sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/S8/g' supplemental.doc
-sed -i 's/\[fig:tiledtree\]/S9/g' supplemental.doc
-sed -i 's/\[fig:c1\]/S10/g' supplemental.doc
-sed -i 's/\[fig:copy\]/S11/g' supplemental.doc
-sed -i 's/\[fig:foodweb\]/S12/g' supplemental.doc
+sed -i 's/\[tab:xyl\]/S1/g' supplemental.doc
+sed -i 's/\[tab:cell\]/S2/g' supplemental.doc
+
+sed -i 's/\[fig:setup\]/S1/g' supplemental.doc
+sed -i 's/\[fig:13C\]/S2/g' supplemental.doc
+sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S3/g' supplemental.doc
+sed -i 's/\[fig:time\]/S4/g' supplemental.doc
+sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S5/g' supplemental.doc
+sed -i 's/\[fig:babund\]/S6/g' supplemental.doc
+sed -i 's/\[fig:genspec\]/S7/g' supplemental.doc
+sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S8/g' supplemental.doc
+sed -i 's/\[fig:example\]/S9/g' supplemental.doc
+sed -i 's/\[fig:copy\]/S10/g' supplemental.doc
+sed -i 's/\[fig:c1\]/S11/g' supplemental.doc
+sed -i 's/\[fig:c2\]/S12/g' supplemental.doc

f863259d8919f31a6445bc347bedcb85950ac4d7
chuckpr <chuck.peperanney@gmail.com>
2015-10-01 10:04:59 -0400

    fixed underscores removed sig statement

--- latex2docx.sh
+++ latex2docx.sh
@@ -4,7 +4,7 @@ csl="pnas.csl"
 
 bash captions.sh
 
-cat Abstract.tex Significance.tex Introduction.tex Results.tex Discussion.tex Methods.tex captions.tex supp_captions.tex \
+cat Abstract.tex Introduction.tex Results.tex Discussion.tex Methods.tex captions.tex supp_captions.tex \
     | pandoc -o manuscript.doc -r latex --bibliography $bib --csl $csl
 
 cat supplemental.tex supp_captions.tex \
@@ -15,7 +15,8 @@ cat supplemental.tex supp_captions.tex \
 sed -i 's/\[fig:ord\]/1/g' manuscript.doc
 sed -i 's/\[fig:l2fc\]/2/g' manuscript.doc
 sed -i 's/\[fig:tiledtree\]/3/g' manuscript.doc
-sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/4/g' manuscript.doc
+#sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/4/g' manuscript.doc
+sed -i 's/\[fig:xyl_count\]/4/g' manuscript.doc
 sed -i 's/\[fig:shift\]/5/g' manuscript.doc
 
 sed -i 's/\[tab:xyl\]/S1/g' manuscript.doc
@@ -23,12 +24,15 @@ sed -i 's/\[tab:cell\]/S2/g' manuscript.doc
 
 sed -i 's/\[fig:setup\]/S1/g' manuscript.doc
 sed -i 's/\[fig:13C\]/S2/g' manuscript.doc
-sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S3/g' manuscript.doc
+#sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S3/g' manuscript.doc
+sed -i 's/\[fig:bulk_ord\]/S3/g' manuscript.doc
 sed -i 's/\[fig:time\]/S4/g' manuscript.doc
-sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S5/g' manuscript.doc
+#sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S5/g' manuscript.doc
+sed -i 's/\[fig:time_class\]/S5/g' manuscript.doc
 sed -i 's/\[fig:babund\]/S6/g' manuscript.doc
 sed -i 's/\[fig:genspec\]/S7/g' manuscript.doc
-sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S8/g' manuscript.doc
+#sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S8/g' manuscript.doc
+sed -i 's/\[fig:rspndr_count\]/S8/g' manuscript.doc
 sed -i 's/\[fig:example\]/S9/g' manuscript.doc
 sed -i 's/\[fig:copy\]/S10/g' manuscript.doc
 sed -i 's/\[fig:c1\]/S11/g' manuscript.doc
@@ -37,7 +41,8 @@ sed -i 's/\[fig:c2\]/S12/g' manuscript.doc
 sed -i 's/\[fig:ord\]/1/g' supplemental.doc
 sed -i 's/\[fig:l2fc\]/2/g' supplemental.doc
 sed -i 's/\[fig:tiledtree\]/3/g' supplemental.doc
-sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/4/g' supplemental.doc
+#sed -i 's/\[fig:xyl<sub>c<\/sub>ount\]/4/g' supplemental.doc
+sed -i 's/\[fig:xyl_count\]/4/g' supplemental.doc
 sed -i 's/\[fig:shift\]/5/g' supplemental.doc
 
 sed -i 's/\[tab:xyl\]/S1/g' supplemental.doc
@@ -45,12 +50,15 @@ sed -i 's/\[tab:cell\]/S2/g' supplemental.doc
 
 sed -i 's/\[fig:setup\]/S1/g' supplemental.doc
 sed -i 's/\[fig:13C\]/S2/g' supplemental.doc
-sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S3/g' supplemental.doc
+#sed -i 's/\[fig:bulk<sub>o<\/sub>rd\]/S3/g' supplemental.doc
+sed -i 's/\[fig:bulk_ord\]/S3/g' supplemental.doc
 sed -i 's/\[fig:time\]/S4/g' supplemental.doc
-sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S5/g' supplemental.doc
+#sed -i 's/\[fig:time<sub>c<\/sub>lass\]/S5/g' supplemental.doc
+sed -i 's/\[fig:time_class\]/S5/g' supplemental.doc
 sed -i 's/\[fig:babund\]/S6/g' supplemental.doc
 sed -i 's/\[fig:genspec\]/S7/g' supplemental.doc
-sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S8/g' supplemental.doc
+#sed -i 's/\[fig:rspndr<sub>c<\/sub>ount\]/S8/g' supplemental.doc
+sed -i 's/\[fig:rspndr_count\]/S8/g' supplemental.doc
 sed -i 's/\[fig:example\]/S9/g' supplemental.doc
 sed -i 's/\[fig:copy\]/S10/g' supplemental.doc
 sed -i 's/\[fig:c1\]/S11/g' supplemental.doc
